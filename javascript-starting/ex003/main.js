3195601a8bc84413e1ac17693869d7db7a7c91db
Leandro Barreto <166774589+leandrobarreto23@users.noreply.github.com>
2024-07-11 15:39:26 -0300

    add function checkguess

--- javascript-starting/ex003/main.js
+++ javascript-starting/ex003/main.js
@@ -17,11 +17,42 @@ inputSubmit.addEventListener('click', checkGuess)
 
 function checkGuess(event) {
   event.preventDefault()
-  if (inputGuess.value ==  randomNumber) {
-    alert("você acertou em " + xAttempts + " tentativas")
-  } else {
-    alert("Você errou")
-    attempts.textContent = "Tentativas: " + xAttempts
-    xAttempts++
+
+  //armazena o valor do input que o usuario palpitou
+  let guessUser = Number(inputGuess.value)
+
+  if(guessUser === 1) {
+    attempts.textContent = "Palpites anteriores: "
   }
+  // Pegue o <p> de resultado e armazene o valor do ultimo palpite
+  attempts.textContent += guessUser + ", "
+
+  // se o palpite do usuario for igual ao numero aleatório escreva...
+  if(guessUser === randomNumber) {
+    lastResult.textContent = "Parabéns! Você acertou!"
+    lastResult.style.backgroundColor = "lightgreen"
+    lowOrHigh.textContent = " "
+    configEndGame()
+  } 
+  // se o numero de tentativas chegar a 5, termine o jogo
+  else if (attempts === 5) {
+    lastResult.textContent = "!!! Fim de Jogo !!!"
+    lowOrHigh.textContent = " "
+    configEndGame()
+  }
+  // se errar o palpite, escreva errado e destaque com uma cor
+  else {
+    lastResult.textContent = "Errado!"
+    lastResult.style.backgroundColor = "lightsalmon"
+    // se o palpite do usuario for menor que o numero aleatório
+    if (guessUser < randomNumber) {
+      lowOrHigh.textContent = "Seu palpite está baixo."
+    } else {
+      lowOrHigh.textContent = "Seu palpite está alto."
+    }
+  }
+  // zera o valor do input quando termina a aplicação
+  inputGuess.value = ""
+  // foca no input no final da aplicação
+  inputGuess.focus()
 }
